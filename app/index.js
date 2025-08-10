9fd6bd49299e3aa56fe53f402a43a55cf499a5b4
zantz <57014873+zantz@users.noreply.github.com>
2025-08-10 15:36:25 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
 export default function App() { return null; }
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello Expo SDK 53 👋</Text>
+    </View>
+  );
+}
